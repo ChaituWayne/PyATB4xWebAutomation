6a03da081cd3e248178d7737e481de9a7417ac94
ChaituWayne <velpurichaitanya@gmail.com>
2024-10-20 20:38:36 +0530

    commit 2

--- src/testcases/test_registration.py
+++ src/testcases/test_registration.py
@@ -1,16 +1,21 @@
+import time
 from selenium import webdriver
 import pytest
 import allure
 from selenium.webdriver.common.by import By
 
 
-@allure.title("Register User")
+@allure.title("Register With Valid User")
 @allure.description("Validate whether User is registered or not")
 @allure.testcase("TC_01")
-@pytest.mark.positive
-def test_resister_user():
+@pytest.mark.regression
+def test_valid_user_registration():
     # Create WebDriver instance for the browser
     driver = webdriver.Chrome()
+    # Maximize window
+    driver.maximize_window()
+    # Add Implicit wait i.e. Global wait, value should be provided in seconds
+    driver.implicitly_wait(10)
     # Navigate to URL
     driver.get("https://awesomeqa.com/ui/index.php?route=account/register")
     # Input First Name in the First Name Text box
@@ -18,7 +23,7 @@ def test_resister_user():
     # Input Last Name in the Last Name Text box
     driver.find_element(By.NAME, "lastname").send_keys("Raisaar")
     # Input email id in the Text box
-    driver.find_element(By.XPATH, "//input[@type='email]").send_keys("khansaarkasalaar@gmail.com")
+    driver.find_element(By.XPATH, "//input[@type='email']").send_keys("khansaarkasalaar@gmail.com")
     # Input telephone number in the Text box
     driver.find_element(By.CSS_SELECTOR, "input[name='telephone']").send_keys("9848032919")
     # Input password in the password text box
@@ -28,4 +33,5 @@ def test_resister_user():
     # select privacy policy checkbox
     driver.find_element(By.XPATH, "//input[@type='checkbox']").click()
     # Click on Continue button
-    driver.find_element(By.XPATH, "//input[@value='continue']").click()
+    driver.find_element(By.XPATH, "//input[@value='Continue']").click()
+    time.sleep(7)
